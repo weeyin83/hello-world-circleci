5b42a704b52fac4eba9d924a0f06cffbbbf97610
Sarah Lean <13692824+weeyin83@users.noreply.github.com>
2022-11-20 10:41:53 +0000

    initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+using Console = Colorful.Console;
+
+namespace circleci_demo_windows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!", Color.Green);
+        }
+    }
+}
